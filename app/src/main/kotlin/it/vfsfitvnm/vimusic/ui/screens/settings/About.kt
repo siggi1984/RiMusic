7f2d6c8d9f5fd9036d3380eda2da83891582ea7a
Daniel <daniel-sevillano@hotmail.com>
2024-03-01 17:32:02 +0100

    Adding app name and version to about page

--- app/src/main/kotlin/it/vfsfitvnm/vimusic/ui/screens/settings/About.kt
+++ app/src/main/kotlin/it/vfsfitvnm/vimusic/ui/screens/settings/About.kt
@@ -1,7 +1,10 @@
 package it.vfsfitvnm.vimusic.ui.screens.settings
 
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
@@ -11,8 +14,11 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import it.vfsfitvnm.vimusic.R
@@ -22,6 +28,7 @@ import it.vfsfitvnm.vimusic.ui.styling.Dimensions
 @Composable
 fun About() {
     val uriHandler = LocalUriHandler.current
+    val context = LocalContext.current
 
     Column(
         modifier = Modifier
@@ -29,6 +36,29 @@ fun About() {
             .verticalScroll(rememberScrollState())
             .padding(vertical = 16.dp)
     ) {
+        Row(
+            modifier = Modifier
+                .padding(horizontal = 16.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            val packageManager = context.packageManager
+            val packageName = context.packageName
+            val packageInfo = packageManager.getPackageInfo(packageName, 0)
+
+            Image(
+                painter = painterResource(id = R.drawable.app_icon),
+                contentDescription = stringResource(id = R.string.app_name)
+            )
+
+            Text(
+                text = "${stringResource(id = R.string.app_name)} v${packageInfo.versionName}",
+                style = MaterialTheme.typography.bodyLarge
+            )
+        }
+
+        Spacer(modifier = Modifier.height(Dimensions.spacer + 8.dp))
+
         Text(
             text = stringResource(id = R.string.social),
             modifier = Modifier.padding(horizontal = 16.dp),
