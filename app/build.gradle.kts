96f55aea01e58f69a60c0e04347ecdec553a2277
Daniel <daniel-sevillano@hotmail.com>
2024-03-05 14:52:26 +0100

    Updating to a new version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.github.musicyou"
         minSdk = 21
         targetSdk = 34
-        versionCode = 1
-        versionName = "0.1"
+        versionCode = 2
+        versionName = "0.2"
     }
 
     splits {
