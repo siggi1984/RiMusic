37a029fd4da4ce65e77bb86acb7efcbed578dc2c
Daniel <daniel-sevillano@hotmail.com>
2024-03-09 11:25:37 +0100

    Simplifying shimmer effect

--- app/src/main/kotlin/it/vfsfitvnm/vimusic/ui/components/ShimmerHost.kt
+++ app/src/main/kotlin/it/vfsfitvnm/vimusic/ui/components/ShimmerHost.kt
@@ -6,11 +6,6 @@ import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawWithContent
-import androidx.compose.ui.graphics.BlendMode
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
 import com.valentinilk.shimmer.shimmer
 
 @Composable
@@ -23,16 +18,7 @@ fun ShimmerHost(
     Column(
         horizontalAlignment = horizontalAlignment,
         verticalArrangement = verticalArrangement,
-        modifier = modifier
-            .shimmer()
-            .graphicsLayer(alpha = 0.99f)
-            .drawWithContent {
-                drawContent()
-                drawRect(
-                    brush = Brush.verticalGradient(listOf(Color.Black, Color.Transparent)),
-                    blendMode = BlendMode.DstIn
-                )
-            },
+        modifier = modifier.shimmer(),
         content = content
     )
 }
